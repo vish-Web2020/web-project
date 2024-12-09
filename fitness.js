922c0fdc7b5388c5a26a453042784423b0b11e5a
vish-Web2020 <vishahlawat2222@gmail.com>
2024-12-09 18:14:35 +0530

    Add files via upload

---
+++ fitness.js
@@ -0,0 +1,21 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+// Tying Text
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Physical Fitness', 'Weight Gain','Strength Training','Fat Lose','Weight Lifting','Running'],
+    typeSpeed: 60,
+    backSpeed:60,
+    backDelay:1000,
+    loop:true,
+  });
